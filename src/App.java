56a11c7f7a8d337895868faff2434660ab3e563f
Maxim <pankrukhin.maxim@gmail.com>
2022-10-02 00:45:02 +0300

    Реализована выборка файлов и получение пути выбранной файла

--- src/App.java
+++ src/App.java
@@ -1,38 +1,81 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.Customizer;
+import java.io.File;
+import java.io.IOException;
 
 public class App extends JPanel {
     App(){
         setSize(1080,720);
-        setBackground(new Color(131, 179, 179));
+        setBackground(new Color(244, 249, 244));
         add(new AddFile());
-        add(new AddField());
         setLayout(null);
         setVisible(true);
     }
 }
 
-class AddFile extends JButton{
+class AddFile extends JButton implements ActionListener{
+
+    JFileChooser fc;
+    File SelFile;
+
     AddFile(){
-        setSize(100, 25);
-        setLocation(10, 10);
-        setText("Добавить");
-        setForeground(new Color(190, 220, 210));
+        //Создаем объект класса библиотеки пользовательских шрифтов
+        customFontColor fontsLib = new customFontColor();
+
+        setSize(300, 60);
+        setLocation(50, 50);
+        setText("Выберите файл");
+
+        setFont(fontsLib.FiraSans_Bold);
+        setForeground(new Color(70, 70, 70));
         setOpaque(true);
+        setHorizontalAlignment(SwingConstants.LEFT);
+        setBackground(new Color(138, 174, 146));
 
-        setBackground(new Color(50, 90, 100));
-
+        fc = new JFileChooser();
         setFocusPainted(false);
-
-        setVisible(true);
+        addActionListener(this);
     }
 
+
+    //Метод, который возвращает путь к выбранному файлу
+    String Select(){
+        SelFile = fc.getSelectedFile();
+        setVisible(false);
+        return SelFile.getPath();
+    }
+
+    //Метод выбора файла из файловой локальной системы
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        int result = fc.showOpenDialog(AddFile.this);
+        System.out.println(Select());
+
+    }
 }
 
-class AddField extends JTextField{
-    AddField(){
-        setSize(300, 25);
-        setLocation(130, 10);
-        setBackground(new Color(190, 220, 210));
+//Класс пользовательских шрифтов
+class customFontColor{
+    Font FiraSans_Black, FiraSans_BlackItalic,
+            FiraSans_Bold, FiraSans_BoldItalic,
+            FiraSans_ExtraBold, FiraSans_ExtraBoldItalic, FiraSans_ExtraLight, FiraSans_ExtraLightItalic,
+            FiraSans_Italic, FiraSans_Light, FiraSans_LightItalic,
+            FiraSans_Medium, FiraSans_MediumItalic,
+            FiraSans_Regular, FiraSans_SemiBold, FiraSans_SemiBoldItalic,
+            FiraSans_Thin, FiraSans_ThinItalic;
+    customFontColor(){
+        try {
+            FiraSans_Bold = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fonts/FiraSans-Bold.ttf")).deriveFont(20f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(FiraSans_Bold);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(FontFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
-        Main mainFrame = new Main();
+        new Main();
     }
 
     Main(){
