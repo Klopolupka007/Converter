02c4574f0dec701786bc123eeb3ecb1f76165cf8
Maxim <pankrukhin.maxim@gmail.com>
2022-10-02 03:19:58 +0300

    Изменен основной дизайн. Из текущего функционала: – Выбор картинки из каталога файлов (проверки на изображение нет). – Предпросмотр изображения на месте изображения по-умолчанию (размер изображения искажается - нужно предусмотреть относительное изменение размера).

--- src/App.java
+++ src/App.java
@@ -2,17 +2,50 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
 import java.beans.Customizer;
 import java.io.File;
 import java.io.IOException;
 
 public class App extends JPanel {
+
+    ImageIcon picture = new ImageIcon(new ImageIcon("src/StandartImage.png").getImage().getScaledInstance(600, 400, Image.SCALE_DEFAULT));
+    JLabel img;
     App(){
         setSize(1080,720);
-        setBackground(new Color(244, 249, 244));
-        add(new AddFile());
+        setBackground(new Color(191, 80, 60));
+        View();
+
+        add(new AddFile(img, picture));        add(new pan());
         setLayout(null);
         setVisible(true);
+    }
+
+
+    //Основной дизайн и вид страницы приложения
+    void View(){
+        img = new JLabel(picture);
+        img.setBackground(Color.gray);
+        img.setBounds(50, 50, 600, 400);
+        add(img);
+    }
+
+}
+
+class pan extends JPanel{
+    boolean state = true;
+    pan(){
+        setSize(1080, 220);
+        setLocation(0, 500);
+        setVisible(true);
+    }
+    public void paint(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+
+            GradientPaint gradientPaint = new GradientPaint(0, 0, new Color(191, 80, 60),
+                    0, 220, new Color(161, 50, 30));
+            g2.setPaint(gradientPaint);
+            g2.fill(new Rectangle2D.Double(0, 0, 1080, 220));
     }
 }
 
@@ -20,31 +53,38 @@ class AddFile extends JButton implements ActionListener{
 
     JFileChooser fc;
     File SelFile;
+    ImageIcon picture;
+    JLabel img;
 
-    AddFile(){
+    AddFile(JLabel img, ImageIcon picture){
+
+        this.img = img;
+        this.picture = picture;
         //Создаем объект класса библиотеки пользовательских шрифтов
         customFontColor fontsLib = new customFontColor();
 
-        setSize(300, 60);
-        setLocation(50, 50);
+        setSize(150, 40);
+        setLocation(275, 470);
         setText("Выберите файл");
 
         setFont(fontsLib.FiraSans_Bold);
-        setForeground(new Color(70, 70, 70));
+        setForeground(new Color(240, 240, 240));
         setOpaque(true);
         setHorizontalAlignment(SwingConstants.LEFT);
-        setBackground(new Color(138, 174, 146));
+        setBackground(new Color(166, 30, 30));
 
         fc = new JFileChooser();
         setFocusPainted(false);
         addActionListener(this);
     }
 
-
     //Метод, который возвращает путь к выбранному файлу
     String Select(){
         SelFile = fc.getSelectedFile();
-        setVisible(false);
+        ImageIcon pic = new ImageIcon(new ImageIcon(SelFile.getPath()).getImage().getScaledInstance(600, 400, Image.SCALE_DEFAULT));
+        img.setIcon(pic);
+        img.setLocation(50, 50);
+
         return SelFile.getPath();
     }
 
@@ -54,7 +94,6 @@ class AddFile extends JButton implements ActionListener{
         fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
         int result = fc.showOpenDialog(AddFile.this);
         System.out.println(Select());
-
     }
 }
 
@@ -69,7 +108,7 @@ class customFontColor{
             FiraSans_Thin, FiraSans_ThinItalic;
     customFontColor(){
         try {
-            FiraSans_Bold = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fonts/FiraSans-Bold.ttf")).deriveFont(20f);
+            FiraSans_Bold = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fonts/FiraSans-Bold.ttf")).deriveFont(15f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(FiraSans_Bold);
         } catch (IOException e) {
